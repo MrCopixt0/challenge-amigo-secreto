ac9596d3658ea8348f4d8c4e0fdf3bda70127561
Anderson <Andexito902@gmail.com>
2025-09-12 02:20:21 -0500

    Actualizado detector de nombres duplicados

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ function agregarNombres() {
     let textoOriginalh2 = "Digite el nombre de sus amigos"
 
     if (nombre === "") {
+        alert("Por favor, inserte un nombre")
         //cambia el "h2"
         titulo.innerText = "Por favor ingrese un nombre correctamente"
         //establece el tiempo de retorno
@@ -19,9 +20,13 @@ function agregarNombres() {
             titulo.innerText = textoOriginalh2;
         }, 2000);
         return;
+    } else if (listaDeNombres.includes(nombre)) {
+        alert('Este nombre ya está en la lista.');
+        return;
     } else {
         //agregamos el nombre a la lista y limpiamos el campo
     listaDeNombres.push(nombre)
     document.getElementById("amigo").value="";
     }
 }
+
