2a67c0ad19fc0ed038b55feca01ad764e5acdca8
Anderson <Andexito902@gmail.com>
2025-09-11 23:17:14 -0500

    Agrega lista y función imput

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+//Lista donde se almacenaran los nombres
+
+let listaDeNombres = [];
+
+function agregarAmigos() {
+    let nombre = document.getElementById("amigo").value;
+}
