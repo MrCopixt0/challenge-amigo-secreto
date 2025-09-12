3482b0badda79f2d75febeada078d8e86f67db2a
Anderson <Andexito902@gmail.com>
2025-09-12 03:32:11 -0500

    Versión Estable que cumple requisitos

--- app.js
+++ app.js
@@ -44,3 +44,30 @@ function mostrarLista() {
         listaDeAmigos.appendChild(li);
     }
 }
+
+//ahora falta agregar la función que sortee los nombres en la lista, una función de reinicio, y una para eliminar nombres especificos.
+
+//Implementa una función para sortear los amigos
+
+function sortearAmigo() {
+    let nombre = document.getElementById("amigo").value;
+    let titulo = document.getElementById("encabezado2");
+    let textoOriginalh2 = "Digite el nombre de sus amigos"
+
+     if (listaDeNombres.length === 0) {
+        alert ("No hay amigos para sortear, agrege al menos uno primero");
+        //cambia el "h2"
+        titulo.innerText = "Por favor ingrese un número primero"
+        //establece el tiempo de retorno
+        setTimeout(() => {
+            titulo.innerText = textoOriginalh2;
+        }, 2000);
+     }
+     //generar un indice random
+     let randomizarIndice = Math.floor(Math.random() * listaDeNombres.length);
+     //obtener el nombre randomizado.
+     let amigoSorteado = listaDeNombres[randomizarIndice];
+     //mostremos el resultado
+     let resultado = document.getElementById("resultado")
+     resultado.innerHTML = `<li>${amigoSorteado}</li>`;
+}
