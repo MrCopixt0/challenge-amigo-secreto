c7d1fbd5e10ea33bad52297369be7e03937500a0
Anderson <Andexito902@gmail.com>
2025-09-12 02:09:08 -0500

    Agrega Función agregar amigos y cambio temporal en h2

--- app.js
+++ app.js
@@ -4,6 +4,24 @@
 
 let listaDeNombres = [];
 
-function agregarAmigos() {
+//función para agregar amigos y cambiar el "h2" temporalmente
+
+function agregarNombres() {
     let nombre = document.getElementById("amigo").value;
+    let titulo = document.getElementById("encabezado2");
+    let textoOriginalh2 = "Digite el nombre de sus amigos"
+
+    if (nombre === "") {
+        //cambia el "h2"
+        titulo.innerText = "Por favor ingrese un nombre correctamente"
+        //establece el tiempo de retorno
+        setTimeout(() => {
+            titulo.innerText = textoOriginalh2;
+        }, 2000);
+        return;
+    } else {
+        //agregamos el nombre a la lista y limpiamos el campo
+    listaDeNombres.push(nombre)
+    document.getElementById("amigo").value="";
+    }
 }
